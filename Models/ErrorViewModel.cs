75b1a442f3dfa870f6a43b7c8437a367d66c36b2
sifat <sifat@DESKTOP-AJMMED2>
2020-10-19 17:53:20 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace d3Webpage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
